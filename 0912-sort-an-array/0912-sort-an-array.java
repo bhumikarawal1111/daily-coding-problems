bd3ff10d45a0588c047c6b884793de5c04fcf857
Bhumika Rawal <bhumikarawal.work111@gmail.com>
2026-07-31 19:37:53 +0530

    Time: 28 ms (40.34%), Space: 86.4 MB (33.71%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -15,30 +15,6 @@ class Solution {
         mergeSort(arr, mid+1, ei);
         merge(arr, si,mid, ei);
     }
-
-    public int partition(int[] arr, int si, int ei) {
-        int pivot = arr[ei];
-        int i = si - 1;
-
-        for (int j = si; j < ei; j++) {
-            if (arr[j] <= pivot) {
-                i++;
-
-                // Swap arr[i] and arr[j]
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-
-        // Place pivot in correct position
-        i++;
-        int temp = arr[i];
-        arr[i] = arr[ei];
-        arr[ei] = temp;
-
-        return i;
-    }
     public static void merge(int arr[], int si, int mid, int ei){
          int i=si;//left
          int j=mid+1;//right
